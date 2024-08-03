b7f502792e4e193ad358630192f1af2ab8536b5d
dankigoda <iordandimanin15@yahoo.com>
2024-08-03 22:51:21 +0300

    Adding a new Sing up page

---
+++ Projects/Sing up -v2/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primery': '1E88E6',
+        'c-black': '22272b'
+      }
+    },
+  },
+  plugins: [],
+}
+
